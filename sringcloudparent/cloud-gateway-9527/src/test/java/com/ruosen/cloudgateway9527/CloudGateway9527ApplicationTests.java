dadbd9515cba569958d46768add7e522e25c83ef
罗先波 <18687269789@163.com>
2020-11-10 19:44:20 +0800

    add gateway

---
+++ sringcloudparent/cloud-gateway-9527/src/test/java/com/ruosen/cloudgateway9527/CloudGateway9527ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruosen.cloudgateway9527;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGateway9527ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
